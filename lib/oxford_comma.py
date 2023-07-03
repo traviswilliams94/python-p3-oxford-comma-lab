d035d7421086bdc132403e5b1b0f006d6108a2f2
Travis <travis.williams94@gmail.com>
2023-07-03 13:59:04 -0600

    complete lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,8 @@
 def oxford_comma(items):
-    return None
+    if len(items) > 1:
+        items[-1] = "and " + items[-1]
+    if len(items)  > 2:
+        return ', '.join(items)
+    else:
+        return ' '.join(items)
+        
